9b477d584b9b8caed1e480d076c573c7532dc043
maureen-james <manjax97@gmail.com>
2022-03-05 01:44:58 +0300

    add conditions for the form validation

--- index.js
+++ index.js
@@ -5,12 +5,34 @@ var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 
 function validate() {
  var year = document.getElementById ("birthyear");
- if(document.myForm.year.value == "" || document.myForm.year.value.length !=4 || document.myForm.year.value >2100 || document.myForm.year.value<=1900)
+ if(document.myForm.year.value == "" || document.myForm.year.value.length !=4 
+ || document.myForm.year.value >2100 || document.myForm.year.value<=1900)
  {
     alert( "Please provide a valid year of birth! eg 2019" );
     document.myForm.year.focus() ;
     return false;
  }
 
+
+ else if( document.myForm.month.value == "" || isNaN( document.myForm.month.value ) || 
+  document.myForm.month.value.length != 2 || document.myForm.month.value > 12  || document.myForm.month.value <= 0){
+     alert( "Please provide your month of birth! between 1 and 12" );
+     document.myForm.month.focus() ;
+     return false;
+}
+
+else if( document.myForm.date.value == "" || isNaN( document.myForm.month.value ) || 
+document.myForm.month.value.length != 2 || document.myForm.date.value > 31 || document.myForm.date.value <= 0){
+   alert( "Please provide your date of birth! between 1 and 12" );
+   document.myForm.month.focus() ;
+   return false;
+}
+else if(genders[0].checked==false && genders[1].checked==false ) {
+    alert("You must select male or female");
+    return false;
+}
+else{
+    return true;
+}
 
 }
