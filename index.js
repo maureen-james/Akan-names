5068d797ecc59c31e151f14151a05316c051a468
maureen-james <manjax97@gmail.com>
2022-03-05 22:10:15 +0300

    bug:debug the output

--- index.js
+++ index.js
@@ -9,22 +9,30 @@ form.addEventListener("submit", (e) =>{
     e.preventDefault();
     let userGender = form.gender.value;
 
+    console.log(`
+    ${birthYear.value}
+    ${birthMonth.value}
+    ${birthDay.value}
+    ${userGender}
+    `)
+
     var akanName = compareNames(birthYear.value, birthMonth.value, birthDay.value, userGender)
 
     displayAkan.innerHTML = `Your Akan Name is: ${akanName}`;
-
-    
 });
 function compareNames(birthyear, birthmonth, birthday, gender){
     var dayNames =["Sunday", "Monday","Tuesday", "Wednesday", "Thursday","Friday","Saturday" ];
     var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
     var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
      if(gender ==="male"){
-         var date = new Date (parseInt(birthyear), parseInt(birthmonth), parseInt(birthday))
+         var date = new Date(birthyear-birthmonth-birthday)
          var dayOfWeek = dayNames[date.getDay()];
+         console.log(`
+        ${dayOfWeek} 
+         `)
          return maleNames[date.getDay()]
         }else{
-            var date = new Date(parseInt(birthyear), parseInt(birthmonth), parseInt(birthday))
+            var date = new Date (birthyear-birthmonth-birthday)
             var dayOfWeek = dayNames[date.getDay()];
           return femaleNames[date.getDay()]
         }
